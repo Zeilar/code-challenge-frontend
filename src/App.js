fe7a0e4e9d22aebfd7b2e39c8bd04ef63a33b6c1
Philip <philip_angelin@hotmail.com>
2021-03-20 01:51:44 +0100

    React Query setup, images container and site "banner"

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
+import "./normalize.css";
+import styled from "styled-components";
+import Images from "./Images";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const Banner = styled.header`
+	background-color: rgb(25 25 25);
+	padding: 50px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const BannerTitle = styled.h1`
+	color: rgb(235 235 235);
+`;
+
+const queryClient = new QueryClient();
+
 export default function App() {
-	return <div className="App">Hello world</div>;
+	return (
+		<div className="App">
+			<Banner>
+				<BannerTitle>Unsplash images</BannerTitle>
+			</Banner>
+			<QueryClientProvider client={queryClient}>
+				<Images />
+			</QueryClientProvider>
+		</div>
+	);
 }
